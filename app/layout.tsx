069c69aabb6c87d02574a502e7a27aae6b6493ab
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2024-01-24 11:49:21 +0100

    add footer to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -24,6 +25,7 @@ export default function RootLayout({
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
+        <Footer/>
       </body>
     </html>
   );
---
+++ components/Footer/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import SignOut from "../SignOut";
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col p-4 justify-center">
+      <div className="flex flex-row p-2 justify-evenly">
+        <section className="flex flex-col">
+          <p>Sponsors:</p>
+          <p>Animals Friends</p>
+          <p>Hardware Store</p>
+        </section>
+        <section className="flex flex-col">
+          <Link href="/admin">Admin</Link>
+          <SignOut />
+        </section>
+      </div>
+      <p className="text-center">Name of catshelter | Page build by <Link href="https://github.com/JTKsson" target="_blank">Timothy Karlsson</Link></p>
+    </div>
+  );
+};
+
+export default Footer
